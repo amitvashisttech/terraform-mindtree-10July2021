897ec97393aa5e2b3ceb1435a2220f6f1e11957b
Your Name <root@your.domain.com>
2021-07-16 07:25:09 +0000

    13-DataSource

--- 16-Backend-and-RemoteState/Remote/backend.tf
+++ 16-Backend-and-RemoteState/Remote/backend.tf
@@ -1,6 +1,6 @@
 terraform { 
   backend "s3" { 
-   bucket = "terraform-demo-av-30oct2020"
+   bucket = "terraform-demo-av-16july2021"
    key = "mterraform01/terraform.tfstate"
    region = "us-east-1"
  }
